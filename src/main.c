5da7a7eb7105568a54da3ad7c7431bcff64faf68
inixyz <alexandru.ene2702@gmail.com>
2023-08-11 14:40:54 +0300

    dram load

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
-#include <stdio.h>
+#include <stdint.h>
 
-int main(){
-	printf("Hello, World!\n");
+struct Cpu{
+	uint32_t register_file[32];
+	uint8_t* memory;
+};
+
+uint32_t dram_load(uint32_t addr, uint8_t size){
+	
 }
+
+int main(){}
