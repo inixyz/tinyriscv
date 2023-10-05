41cacd9ae5854a69730dfd3ae3b5255458c66b00
inixyz <alexandru.ene2702@gmail.com>
2023-10-06 00:27:09 +0300

    ram load test

--- src/main.c
+++ src/main.c
@@ -17,6 +17,20 @@ uint32_t tinyrv_ram_load(const tinyrv_cpu* cpu, const uint32_t addr,
 	return val;
 }
 
-int main(){
+#include <stdio.h>
 
+int main(){
+	uint8_t memory[100];
+	tinyrv_cpu cpu;
+	cpu.memory = memory;
+
+	memory[0] = 0xAA;
+	memory[1] = 0xBB;
+	memory[2] = 0xCC;
+	memory[3] = 0xDD;
+
+	printf("%x\n", tinyrv_ram_load(&cpu, 0, 1));
+	printf("%x\n", tinyrv_ram_load(&cpu, 0, 2));
+	printf("%x\n", tinyrv_ram_load(&cpu, 0, 3));
+	printf("%x\n", tinyrv_ram_load(&cpu, 0, 4));
 }
