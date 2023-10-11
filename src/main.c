d65e4f5d30bfdee1d27b530041be3add39f348bc
inixyz <alexandru.ene2702@gmail.com>
2023-10-11 18:36:54 +0300

    dump registers

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
 #include "tinyriscv.h"
+#include <stdio.h>
 
-int main(){
+void dump_registers(const tinyriscv_Cpu* cpu){
+	printf("pc = 0x%.8x\n", cpu->pc);
 
+	printf("\n");
+
+	for(unsigned int i = 0; i < 32; i++){
+		printf("r%.2d = 0x%.8x\t", i, cpu->regs[i]);
+		if(i % 2) printf("\n");
+	}
+}
+
+int main(int argc, char** argv){
+	tinyriscv_Cpu cpu;
+	tinyriscv_init(&cpu);
+
+	
+
+	dump_registers(&cpu);
 }
